4929afa26499eabd2f553de7a48e42dbae06d069
SnackerPirater <lolbitz2002@gmail.com>
2025-03-15 16:24:44 -0700

    disable tornado spell for this next update, it takes SO MUCH EFFORT

--- src/main/java/com/snackpirate/aeromancy/data/AAUpgradeOrbs.java
+++ src/main/java/com/snackpirate/aeromancy/data/AAUpgradeOrbs.java
@@ -3,27 +3,13 @@ package com.snackpirate.aeromancy.data;
 import com.snackpirate.aeromancy.Aeromancy;
 import com.snackpirate.aeromancy.item.AAItems;
 import com.snackpirate.aeromancy.spells.AASpells;
-import io.redspace.ironsspellbooks.IronsSpellbooks;
-import io.redspace.ironsspellbooks.api.registry.AttributeRegistry;
 import io.redspace.ironsspellbooks.item.armor.UpgradeOrbType;
-import io.redspace.ironsspellbooks.registries.ItemRegistry;
 import io.redspace.ironsspellbooks.registries.UpgradeOrbTypeRegistry;
-import net.minecraft.core.Registry;
-import net.minecraft.core.RegistryAccess;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.neoforged.neoforge.registries.DataPackRegistryEvent;
 
 public class AAUpgradeOrbs {
-
-	public static Registry<UpgradeOrbType> upgradeTypeRegistry(RegistryAccess registryAccess) {
-		return registryAccess.registryOrThrow(UpgradeOrbTypeRegistry.UPGRADE_ORB_REGISTRY_KEY);
-	}
-
-	public static void registerDatapackRegistries(DataPackRegistryEvent.NewRegistry event) {
-		event.dataPackRegistry(UpgradeOrbTypeRegistry.UPGRADE_ORB_REGISTRY_KEY, UpgradeOrbType.CODEC, UpgradeOrbType.CODEC);
-	}
 
 	public static ResourceKey<UpgradeOrbType> WIND_SPELL_POWER = ResourceKey.create(UpgradeOrbTypeRegistry.UPGRADE_ORB_REGISTRY_KEY, Aeromancy.id("wind_spell_power"));
 
@@ -31,4 +17,4 @@ public class AAUpgradeOrbs {
 		bootstrap.register(WIND_SPELL_POWER,
 				new UpgradeOrbType(AASpells.Attributes.WIND_SPELL_POWER, 0.05, AttributeModifier.Operation.ADD_MULTIPLIED_BASE, AAItems.WIND_UPGRADE_ORB));
 	}
-	}
+}
--- src/main/java/com/snackpirate/aeromancy/spells/AASpells.java
+++ src/main/java/com/snackpirate/aeromancy/spells/AASpells.java
@@ -79,7 +79,7 @@ public class AASpells {
 	public static final Supplier<AbstractSpell> WIND_BLADE = registerSpell(new WindBladeSpell());
 	public static final Supplier<AbstractSpell> FLUSH = registerSpell(new FlushSpell());
 	public static final Supplier<AbstractSpell> DASH = registerSpell(new DashSpell());
-	public static final Supplier<AbstractSpell> TORNADO = registerSpell(new TornadoSpell());
+//	public static final Supplier<AbstractSpell> TORNADO = registerSpell(new TornadoSpell());
 //	public static final Supplier<AbstractSpell> SUMMON_BREEZE = registerSpell(new SummonBreezeSpell());
 
 	public static class Entities {
